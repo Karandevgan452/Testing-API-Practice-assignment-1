336cdedae6a585f59201ae7b7dd3d05e49a7600f
name <karankarandevgan@gmail.com>
2025-02-13 09:59:23 +0530

    Assignment completed

--- index.js
+++ index.js
@@ -35,15 +35,32 @@
 const express = require('express');
 const { resolve } = require('path');
 
+const data = require('./data.json');
+
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
-
+// POST /students/above-threshold
+app.post('/students/above-threshold' , (req , res) => {
+  const {threshold} = req.body;
+  if (typeof threshold !== 'number'){
+    return res.status(400).json({message: 'Bad Request'})
+  }
+  const studentData = data.filter((e) => e.total > threshold);
+ res.json ({
+  count: studentData.length,
+  students: studentData.map((e) =>({
+    name: e.name,
+    total: e.total
+  })),
+ });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
